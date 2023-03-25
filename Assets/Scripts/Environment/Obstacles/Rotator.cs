37bfbea01e8440e72a46b3fc0b64f503cd84051c
kalamayin <lingo.zwe@gmail.com>
2023-03-25 18:49:56 +0300

    Engel Update

--- Assets/Scripts/Environment/Obstacles/Rotator.cs
+++ Assets/Scripts/Environment/Obstacles/Rotator.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    [Range (-1, 1)]
+    [SerializeField] int direction;
+
     [SerializeField] float speed;
 
     public static float coeff;
@@ -11,12 +14,29 @@ public class Rotator : MonoBehaviour
     {
         coeff = 1f;
         StartCoroutine(Delay());
+        //if (direction == 0) SetDirectionRandomly();
     }
 
     void FixedUpdate()
     {
-        if(GameController.gameState == GameState.Playing) transform.Rotate(Vector3.up * speed * coeff);
+        if(GameController.gameState == GameState.Playing) transform.Rotate(Vector3.up * speed * direction * coeff);
     }
+
+    //void SetDirectionRandomly()
+    //{
+    //    int random = random.Range(0, 2);
+    //    switch (random)
+    //    {
+    //        case 0:
+    //            direction = 1;
+    //            break;
+    //        case 1:
+    //            direction = -1;
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
 
     IEnumerator Delay()
     {
